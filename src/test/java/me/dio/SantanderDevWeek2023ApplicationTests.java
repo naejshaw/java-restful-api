ad484d2884324c787f781ee4a8c7dcd7981daf7c
JeanFDev <jeanjfra1@gmail.com>
2023-10-18 13:56:33 -0300

    .

--- src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
+++ src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.javarestfulapi;
+package me.dio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaRestfulApiApplicationTests {
+class SantanderDevWeek2023ApplicationTests {
 
 	@Test
 	void contextLoads() {
